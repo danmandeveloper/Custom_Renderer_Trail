d6be742202e00197ce6ac503980b861bbb6fb6d6
danmandeveloper <91624403+danmandeveloper@users.noreply.github.com>
2022-10-03 14:53:54 -0500

    Monday's Cumulative Work

---
+++ Assets/Scripts/Test_Script.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test_Script : MonoBehaviour
+{
+    public float offset, speed;
+    public AnimationCurve height, movement;
+    public Transform cubeA, cubeB;
+    private float rate;
+    private Vector3 desiredPosition;
+    
+    private void Update()
+    {
+        rate += Time.deltaTime * speed;
+
+        desiredPosition = Vector3.Lerp(cubeA.position, cubeB.position, movement.Evaluate(rate));
+        desiredPosition.y += height.Evaluate(rate) + offset;
+        transform.position = desiredPosition;
+
+        if (rate >= 1)
+        {
+            rate = 0;
+            //desiredPosition = Vector3.Lerp(cubeB.position, cubeA.position, movement.Evaluate(rate));
+            //desiredPosition.y += height.Evaluate(rate) + offset;
+            //transform.position = desiredPosition;
+        }
+    }
+}
