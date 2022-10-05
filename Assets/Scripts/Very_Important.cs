24b660fe0d1c49d9abdcea1999eedbab9582af55
danmandeveloper <91624403+danmandeveloper@users.noreply.github.com>
2022-10-05 15:09:52 -0500

    Revert "We got Line Renders"
    This reverts commit e1342aad73ad26c67aa3c43c7c5094c3e5ccf95b.

--- Assets/Scripts/Very_Important.cs
+++ Assets/Scripts/Very_Important.cs
@@ -4,37 +4,15 @@ using UnityEngine;
 
 public class Very_Important : MonoBehaviour
 {
-    [SerializeField]
-    private LineRenderer lRenderer;
-    [SerializeField]
-    private Transform targetA, targetB;
-    [Range(2, 100)]
-    [SerializeField]
-    private int lineRes = 2;
-    [SerializeField]
-    private AnimationCurve curvy;
-    private Vector3 desiredPosition;
-
-    private void Start()
+    // Start is called before the first frame update
+    void Start()
     {
-        lRenderer = GetComponent<LineRenderer>();
+        
     }
+
+    // Update is called once per frame
     void Update()
     {
-        if (lRenderer.positionCount != lineRes)
-        {
-            lRenderer.positionCount = lineRes;
-        }
-        for (int x = 0; x < lRenderer.positionCount; x++)
-        {
-            desiredPosition = Vector3.Lerp(targetA.position, targetB.position, x / (float)lRenderer.positionCount);
-            lRenderer.SetPosition(x, new Vector3(desiredPosition.x, desiredPosition.y + curvy.Evaluate(x / (float)lRenderer.positionCount), desiredPosition.z));
-
-            if (x >= lRenderer.positionCount - 1)
-            {
-                desiredPosition = Vector3.Lerp(targetA.position, targetB.position, 1);
-                lRenderer.SetPosition(x, new Vector3(desiredPosition.x, desiredPosition.y + curvy.Evaluate(x / (float)lRenderer.positionCount), desiredPosition.z));
-            }
-        }
+        
     }
 }
