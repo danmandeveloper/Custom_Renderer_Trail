d256a8f11cfef35c747e54c119c279eda4cd81a4
jesseray1450 <jesse.ray@trojans.dsu.edu>
2022-10-05 14:49:14 -0500

    MovingBlocks

--- Assets/Scripts/BoxBJigglin.cs
+++ Assets/Scripts/BoxBJigglin.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -8,16 +9,14 @@ using UnityEngine.UIElements;
 public class BoxBJigglin : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
-    {
-        Debug.Log("trigger enter");
+    { 
         Vector3 newScale = transform.localScale;
         newScale.y *= 1.2f;            
         transform.localScale = newScale;
-        
-     
     }
     private void OnTriggerExit(Collider other)
     {
+        transform.localScale = new Vector3(1, 1, 1); 
         
     }
 }
