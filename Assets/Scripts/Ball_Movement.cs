8f423552487c9fe0aeea23fc7f1b3890ab135984
jesseray1450 <jesse.ray@trojans.dsu.edu>
2022-10-05 18:17:21 -0500

    trail

--- Assets/Scripts/Ball_Movement.cs
+++ Assets/Scripts/Ball_Movement.cs
@@ -1,17 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+
 
 public class Ball_Movement : MonoBehaviour
 {
+    [SerializeField]
+    public int l;
+    private LineRenderer lrenderer;
+    private Vector3 compA, compB;
+    private Vector3 desiredPosition;
     public float offset, speed;
     public AnimationCurve height, movement;
     public Transform cubeA, cubeB;
     private float rate;
-    private Vector3 desiredPosition;
-    
+
+    private void Start()
+    {
+        lrenderer = GetComponent<LineRenderer>();
+        lrenderer.material.color = Color.magenta;
+    }
+
     private void Update()
     {
+       
         rate += Time.deltaTime * speed;
 
         desiredPosition = Vector3.Lerp(cubeA.position, cubeB.position, movement.Evaluate(rate));
@@ -21,9 +34,14 @@ public class Ball_Movement : MonoBehaviour
         if (rate >= 1)
         {
             rate = 0;
-            //desiredPosition = Vector3.Lerp(cubeB.position, cubeA.position, movement.Evaluate(rate));
-            //desiredPosition.y += height.Evaluate(rate) + offset;
-            //transform.position = desiredPosition;
+        } 
+        lrenderer.positionCount = l;
+        compA = transform.position;
+        for(int i = 0; i< l; i++)
+        {
+            compB = lrenderer.GetPosition(i);
+            lrenderer.SetPosition(i, compA);
+            compA = compB;
         }
     }
 }
