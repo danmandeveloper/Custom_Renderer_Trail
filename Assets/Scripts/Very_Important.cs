e1342aad73ad26c67aa3c43c7c5094c3e5ccf95b
danmandeveloper <91624403+danmandeveloper@users.noreply.github.com>
2022-10-05 14:51:18 -0500

    We got Line Renders

--- Assets/Scripts/Very_Important.cs
+++ Assets/Scripts/Very_Important.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class Very_Important : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField]
+    private LineRenderer lRenderer;
+    [SerializeField]
+    private Transform targetA, targetB;
+    [Range(2, 100)]
+    [SerializeField]
+    private int lineRes = 2;
+    [SerializeField]
+    private AnimationCurve curvy;
+    private Vector3 desiredPosition;
 
-    // Update is called once per frame
+    private void Start()
+    {
+        lRenderer = GetComponent<LineRenderer>();
+    }
     void Update()
     {
-        
+        if (lRenderer.positionCount != lineRes)
+        {
+            lRenderer.positionCount = lineRes;
+        }
+        for (int x = 0; x < lRenderer.positionCount; x++)
+        {
+            desiredPosition = Vector3.Lerp(targetA.position, targetB.position, x / (float)lRenderer.positionCount);
+            lRenderer.SetPosition(x, new Vector3(desiredPosition.x, desiredPosition.y + curvy.Evaluate(x / (float)lRenderer.positionCount), desiredPosition.z));
+
+            if (x >= lRenderer.positionCount - 1)
+            {
+                desiredPosition = Vector3.Lerp(targetA.position, targetB.position, 1);
+                lRenderer.SetPosition(x, new Vector3(desiredPosition.x, desiredPosition.y + curvy.Evaluate(x / (float)lRenderer.positionCount), desiredPosition.z));
+            }
+        }
     }
 }
